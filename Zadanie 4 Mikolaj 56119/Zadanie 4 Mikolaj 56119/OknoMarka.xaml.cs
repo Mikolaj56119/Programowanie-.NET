66b703138940e3cb641d61459ace8d7c9c954510
Mikolaj56119 <93341668+Mikolaj56119@users.noreply.github.com>
2022-07-14 19:06:07 +0200

    Add files via upload

---
+++ Zadanie 4 Mikolaj 56119/Zadanie 4 Mikolaj 56119/OknoMarka.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Zadanie_4_Mikolaj_56119
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class OknoMarka : Window
+    {
+        public OknoMarka(System.Xml.XmlElement marka)
+        {
+            DataContext = marka;
+            Console.WriteLine(marka);
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void WyświetlModele(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement parent = (FrameworkElement)((FrameworkElement)sender).Parent;
+            ListBox lista = (ListBox)parent.FindName("Marka");
+            new OknoModel((System.Xml.XmlElement)lista.SelectedItem).Show();
+        }
+    }
+}
